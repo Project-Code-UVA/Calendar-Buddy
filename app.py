f75530ccce94a08e7ca62363c41401ac8b1c391a
KaylaBtv22 <kaylabutv@gmail.com>
2025-10-18 21:14:21 -0400

    Added and connected css file

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # Imports
-from flask import Flask
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialize Flask app
 app = Flask(__name__)
 
-
 # Home page
 @app.route("/")
 def index():
-    return "HOME"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
