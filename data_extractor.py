8e1377de994b0fdfb2bfc7779c19f341e47bd60d
Josalin <bw15sp@gmail.com>
2025-10-25 16:27:58 -0400

    changes to nlp_extraction, data_extraction, requirements.txt in text_extraction branch

--- data_extractor.py
+++ data_extractor.py
@@ -7,38 +7,8 @@ from date_spacy import find_dates
 
 # main function to test events_regex
 def main():
-    print(events_regex())
-    print(parse_dates())
-
-
-# extract events using regex
-def events_regex(file_name="basic_sample.txt"):
-    events = {}
-    file_path = os.path.join(
-        os.path.dirname(__file__), file_name
-    )  # ensure the correct path to the file
-    with open(file_path) as f:  # opening file
-        for line in f:
-            line = line.strip()  # stripping whitespace
-            # regex pattern to match dates in various formats
-            if match := re.search(
-                r"(\d{1,2}[/-]\d{1,2}[/-]\d{2,4})|([A-Za-z]+ \d{1,2}, \d{4})", line
-            ):
-                date = match.group()
-                before, date, after = line.partition(
-                    date
-                )  # extracting everything before and after the date
-                if before:  # adding entry of date, event_name to events dictionary
-                    event_name = before.strip()
-                    events.update({date: event_name})
-                elif after:  # if nothing before date, check after
-                    event_name = after.strip()
-                    events.update({date: event_name})
-            else:  # if date pattern not matched, print no date found
-                print("No date found")
-    f.close()
-    return events
-
+    print(parse_dates("basic_sample.txt"))
+    print(parse_dates("complicated_sample.txt"))
 
 def parse_dates(file_name="complicated_sample.txt"):
 
--- nlp_extraction.py
+++ nlp_extraction.py
@@ -4,19 +4,23 @@ import dateparser
 import spacy
 
 def main():
-    nlp_extractor()
+    nlp_extractor(open_file("complicated_sample.txt"))
+    nlp_extractor(open_file("basic_sample.txt"))
 
-def nlp_extractor(file_name="complicated_sample.txt"):
-    #Load a natural language processing model with spacy for date parsing
-    nlp = spacy.load("en_core_web_sm")
-
+def open_file(file_name="complicated_sample.txt"):
     file_path = os.path.join(
         os.path.dirname(__file__), file_name
     )  # ensure the correct path to the file
-
     with open(file_path, "r", encoding="utf-8") as f:  # opening file
         #Read the file content and assign labels
         file_content = f.read()
+
+    return file_content
+
+
+def nlp_extractor(file_content):
+    #Load a natural language processing model with spacy for date parsing
+    nlp = spacy.load("en_core_web_sm")
 
     doc = nlp(file_content)
     print("result of model " + str(doc.ents))
