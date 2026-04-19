b4da69ce2ed9ad55ffdc915853fa8f9603f23285
KaylaBtv22 <kaylabutv@gmail.com>
2026-04-18 22:52:10 -0400

    calendar functionality

--- static/script.js
+++ static/script.js
@@ -4,6 +4,7 @@ console.log("calendar js loaded");
 const datetxtEl = document.querySelector('.datetxt');
 const datesEl = document.querySelector('.dates');
 const btnEl = document.querySelectorAll('.calendar_headings .fa-solid');
+const todayBtn = document.getElementById('todayBtn');
 const monthYearEl = document.querySelector('.month_year')
 
 let dmObj = {
@@ -43,4 +44,73 @@ let date = dateObj.getDate();
 
 console.log(dayName, month, year, date);
 
+datetxtEl.innerHTML = `${dayName}, ${dmObj.months[month]} ${date}, ${year}`;
 
+//calendar functionality
+const displayCalendar = ()=>{
+    let firstDayofMonth = new Date (year, month, 1).getDay();
+    let lastDateofLastMonth = new Date (year, month,0).getDate();
+
+    let lastDateofMonth = new Date (year,month+1,0).getDate();
+     let lastDayofMonth = new Date (year, month, lastDateofMonth).getDay();
+    let days = ""; 
+
+    //previous month days
+        for (let i = firstDayofMonth; i>0; i--){
+            days += `<li class ="dummy">${lastDateofLastMonth-i+1}</li>`;
+        }
+        
+    //this month
+    for (i =1; i <= lastDateofMonth; i++){
+
+        let checkToday = 
+        i === dateObj.getDate() && 
+        month === new Date().getMonth() && 
+        year === new Date().getFullYear()
+            ? "today"
+            :"notToday";
+        
+        days += `<li class = ${checkToday}>${i}</li>`;
+     }   
+
+     //next month days
+     for (let i = lastDayofMonth; i <6; i ++){
+         days += `<li class ="dummy">${i-lastDayofMonth+1}</li>`;
+     }
+
+    datesEl.innerHTML = days;
+    monthYearEl.innerHTML = `${dmObj.months[month]} ${year}`;
+}
+
+displayCalendar();
+
+
+//previous and next month buttons
+btnEl.forEach((btns) =>{
+    btns.addEventListener('click', ()=>{
+        month = btns.id ==="prev" ? month -1 : month +1;
+        
+        
+        if (month <0 || month>11){
+            date = new Date (year, month, new Date().getDate());
+            year = date.getFullYear();
+            month = date.getMonth();
+        } else{
+            date = new Date();
+        }
+
+    
+        
+        
+        displayCalendar();
+    });
+});
+
+//today button
+todayBtn.addEventListener('click', ()=>{
+    dateObj = new Date();
+    month = dateObj.getMonth();
+    year = dateObj.getFullYear();
+
+    displayCalendar();
+});
